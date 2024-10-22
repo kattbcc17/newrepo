2d0b5ae92e407f2b5b49c7c80bb37891b3868d03
Kattia <kattbcc17@byui.edu>
2024-10-22 10:52:49 -0300

    MVC implementation

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
@@ -26,9 +27,7 @@ app.set("layout", "./layouts/layout"); // not at views root
 app.use("/", static);
 
 // Index Route
-app.get("/", (_, res) => {
-  res.render("index", { title: "Home" });
-});
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
