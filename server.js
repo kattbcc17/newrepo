f1176da3e43c331bbe666e7e3c34b7c9be1a5219
Kattia <kattbcc17@byui.edu>
2024-10-22 22:58:28 -0300

    Update pnpm-lock.yaml to match package.json

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
 
+
 /* ***********************
  * Middleware Setup
  *************************/
@@ -24,12 +25,30 @@ app.set("layout", "./layouts/layout"); // not at views root
 /* ***********************
  * Routes
  *************************/
-app.use("/", static);
+app.use(static);
+// Index route
+app.get("/", baseController.buildHome)
+
 
 // Index Route
 app.get("/", baseController.buildHome)
-// Inventory routes
-app.use("/inv", inventoryRoute)
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
