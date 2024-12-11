cf040bb89409bb389f276923937961c5c71e13f7
Kattia <kattbcc17@byui.edu>
2024-12-10 22:06:28 -0500

    inventory validation.js

--- utilities/inventory-validation.js
+++ utilities/inventory-validation.js
@@ -36,4 +36,109 @@ validate.classificationRules = () => {
     next()
   }
   
+  validate.inventoryRules = () => {
+    return [
+    
+        body("inv_make")
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage("Make must be at least 3 characters."),
+
+        body("inv_model")
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage("Model must be at least 3 characters."),
+
+        body("inv_description")
+        .trim()
+        .isLength({ min: 10 })
+        .withMessage("Description must be at least 10 characters."),
+        body("inv_price")
+        .trim()
+        .isNumeric()
+        .withMessage("Price must be a number."),
+
+        body("inv_year")
+        .trim()
+        .isInt({min: 1000, max: 9999})
+        .withMessage("Year must be a 4 digit number."),
+
+        body("inv_miles")
+        .trim()
+        .isNumeric()
+        .withMessage("miles must be a number."),
+
+        body("inv_color")
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage("Model must be at least 3 characters."),      
+    ];
+      
+  }
+
+/* ******************************
+ * Check data and return errors or continue to registration
+ * ***************************** */
+validate.checkInventoryData = async (req, res, next) => {
+  const {classification_id, inv_make, inv_model,  inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color} = req.body
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    let select = await utilities.buildClassificationList(classification_id)
+    res.render("inventory/addInventory", {
+      errors,
+      title: "Add New Car",
+      nav,
+      select,
+      classification_id,
+      inv_make,
+      inv_model,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_year,
+      inv_miles,
+      inv_color,
+    })
+    return
+  }
+  next()
+}
+
+
+/* ******************************
+* Check data and return errors to edit view or continue to update car
+* ***************************** */
+validate.checkUpdateData = async (req, res, next) => {
+const {inv_id, classification_id, inv_make, inv_model,  inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color} = req.body
+let errors = []
+errors = validationResult(req)
+if (!errors.isEmpty()) {
+  let nav = await utilities.getNav()
+  let classification = parseInt(classification_id)
+  let dropDown = await utilities.buildClassificationList(classification)
+  res.render("inventory/editInventory", {
+    errors,
+    title: "Edit" + inv_make + " " + inv_model,
+    nav,
+    select:dropDown,
+    classification_id,
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color,
+    inv_id,
+  })
+  return
+}
+next()
+}
+
 module.exports = validate
