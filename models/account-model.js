b83c606903d128864d936af34df11bf88c48da58
Kattia <kattbcc17@byui.edu>
2024-12-10 21:57:16 -0500

    last update for thr model.js

--- models/account-model.js
+++ models/account-model.js
@@ -23,4 +23,68 @@ async function registerAccount(
   }
 }
 
-module.exports = { registerAccount };
+/* **********************
+ *   Check for existing email
+ * ********************* */
+async function checkExistingEmail(account_email) {
+  try {
+    const sql = 'SELECT * FROM account WHERE account_email = $1';
+    const email = await pool.query(sql, [account_email]);
+    return email.rowCount;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+/* *****************************
+ * Return account data using email address
+ * ***************************** */
+async function getAccountByEmail(account_email) {
+  try {
+    const result = await pool.query(
+      'SELECT account_id, account_firstname, account_lastname, account_email, account_type, account_password FROM account WHERE account_email = $1',
+      [account_email]
+    );
+    return result.rows[0];
+  } catch (error) {
+    return new Error('No matching email found');
+  }
+}
+
+/* ***************************
+ *  Update account Data
+ * ************************** */
+async function modelUpdateAccount(
+  account_id,
+  account_firstname,
+  account_lastname,
+  account_email
+) {
+  try {
+    const sql =
+    'UPDATE public.account SET account_firstname = $1, account_lastname = $2, account_email = $3 WHERE account_id = $4 RETURNING *';
+    const data = await pool.query(sql, [
+      account_firstname,
+      account_lastname,
+      account_email,
+      account_id,
+    ])
+    return data.rows[0]
+  } catch (error) {
+    console.error("model error: " + error)
+  }
+}
+
+async function updatePassword(account_password, account_id)
+{
+  const sql =
+  "UPDATE public.account SET account_password = $1 WHERE account_id = $2 RETURNING *"
+  try{
+    const data = await pool.query(sql, [account_password, account_id])
+    return data.rows[0]
+  }catch(error){
+    console.error(error)
+  }
+}
+
+module.exports = { registerAccount, checkExistingEmail, getAccountByEmail, modelUpdateAccount, updatePassword };
